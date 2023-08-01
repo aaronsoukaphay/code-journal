c8b2367967abb4506c6c3ecf96c123f374189790
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2023-08-01 17:36:44 +0000

    In data.js, add a 'beforeunload' event listener to the window object that serializes the data model as JSON and stores the JSON in localStorage.  At the bottom of data.js, include a conditional statement that:  checks to see if there is a JSON data model stored in localStorage.  if there is, parse it and assign it to the initial data variable so that the user can continue adding entries to the pre-existing data model. #1

--- js/data.js
+++ js/data.js
@@ -8,3 +8,16 @@ let data = {
 };
 
 // console.log(data.entries)
+
+window.addEventListener('beforeunload', handleUnload);
+
+function handleUnload() {
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', dataJSON);
+}
+
+const previousDataJSON = localStorage.getItem('javascript-local-storage');
+
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
